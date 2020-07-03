e3ecd107b4e4a879b41d1e5f588f3eb975184e02
erdac <tonrc87@gmail.com>
2020-07-03 20:54:50 -0300

    053-02 Inicio CSS do about

--- src/objects/About/index.js
+++ src/objects/About/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
+import './styles.css';
 
-const About = () => <article className="about">Elton Costa</article>;
+const About = () => <article className="about"></article>;
 
 export default About;
