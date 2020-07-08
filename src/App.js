a40818fd5a40f3584933f751b6966c9228612291
erdac <tonrc87@gmail.com>
2020-07-08 14:01:10 -0300

    053-12 Criado compoment HeaderInternal

--- src/App.js
+++ src/App.js
@@ -3,23 +3,19 @@ import './App.css';
 
 import Checkbox from './objects/Checkbox';
 import About from './objects/About';
-import LogoElton from './objects/LogoElton';
-import AboutLink from './objects/AboutLink';
-import IconClose from './objects/IconClose';
 import Header from './components/Header';
 import Hashtag from './components/Hashtag';
+import HeaderInternal from './components/HeaderInternal';
 
 const App = () => (
     <main className="app">
       <Header />
       <Hashtag />
       <Checkbox id="show" value="show" type="checkbox" content="Mostrar eventos" />
-
       <About>
-        <LogoElton light />
-        <AboutLink className="-light" />
-        <IconClose />
+        <HeaderInternal />
       </About>
+    
     </main>
 );
 
---
+++ src/components/HeaderInternal/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import LogoElton from '../../objects/LogoElton';
+import AboutLink from '../../objects/AboutLink';
+import IconClose from '../../objects/IconClose';
+
+const HeaderInternal = () => (
+    <header className="header-internal">
+        <LogoElton light />
+        <AboutLink className="-light" />
+        <IconClose />
+    </header>
+);
+
+export default HeaderInternal;
