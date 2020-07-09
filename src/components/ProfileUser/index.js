3b1b64057f27c5c4d8e3fefa3fd05b14e1a38a3f
erdac <tonrc87@gmail.com>
2020-07-09 18:23:14 -0300

    053-18 CSS do ProfileUser

--- src/components/ProfileUser/index.js
+++ src/components/ProfileUser/index.js
@@ -1,12 +1,13 @@
 import React from  'react';
+import './styles.css';
 import Profile from '../../img/profile.jpg';
 import AvatarProfile from '../../objects/AvatarProfile';
 
 const ProfileUser = () => (
-    <dl>
-        <dd><AvatarProfile src={Profile} alt="Avatar do Elton Costa" /></dd>
-        <dt>Desenvolvedor Front-End Jr.</dt>
-        <dd>Meu objetivo é encontrar uma oportunidade de emprego, e assim aprofundar e melhorar minhas habilidades em programação.</dd>
+    <dl className="profile-user">
+        <dd className="avatar"><AvatarProfile src={Profile} alt="Avatar do Elton Costa" /></dd>
+        <dt className="title">Desenvolvedor Front-End Jr.</dt>
+        <dd className="description">Meu objetivo é encontrar uma oportunidade de emprego, e assim aprofundar e melhorar minhas habilidades em programação.</dd>
     </dl>
 );
 
