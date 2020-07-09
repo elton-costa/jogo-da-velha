a2ebc11846ed68f4703c276c2ddc156d86475dd1
erdac <tonrc87@gmail.com>
2020-07-09 18:37:15 -0300

    053-19 Adicionado respiros do ProfileUser

--- src/components/ProfileUser/index.js
+++ src/components/ProfileUser/index.js
@@ -6,7 +6,7 @@ import AvatarProfile from '../../objects/AvatarProfile';
 const ProfileUser = () => (
     <dl className="profile-user">
         <dd className="avatar"><AvatarProfile src={Profile} alt="Avatar do Elton Costa" /></dd>
-        <dt className="title">Desenvolvedor Front-End Jr.</dt>
+        <dt className="title">Dev Front-End Jr</dt>
         <dd className="description">Meu objetivo é encontrar uma oportunidade de emprego, e assim aprofundar e melhorar minhas habilidades em programação.</dd>
     </dl>
 );
