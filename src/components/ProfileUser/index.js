d4f27d0fe419c7f0aeb13281e1c259cc27c83cda
erdac <tonrc87@gmail.com>
2020-07-11 20:34:41 -0300

    053-20 Criado o object IconSocial

--- src/components/ProfileUser/index.js
+++ src/components/ProfileUser/index.js
@@ -1,13 +1,21 @@
 import React from  'react';
 import './styles.css';
 import Profile from '../../img/profile.jpg';
+import GitHub from '../../img/github.png';
+import Linkedin from '../../img/linkedin.png';
 import AvatarProfile from '../../objects/AvatarProfile';
+import IconSocial from '../../objects/IconSocial';
 
 const ProfileUser = () => (
     <dl className="profile-user">
         <dd className="avatar"><AvatarProfile src={Profile} alt="Avatar do Elton Costa" /></dd>
         <dt className="title">Dev Front-End Jr</dt>
         <dd className="description">Meu objetivo é encontrar uma oportunidade de emprego, e assim aprofundar e melhorar minhas habilidades em programação.</dd>
+        <dd className="socials" >
+            <IconSocial src={GitHub} alt="Logo do GitHub"/>
+            <IconSocial src={Linkedin} alt="Logo do Linkedin"/>
+
+        </dd>
     </dl>
 );
 
