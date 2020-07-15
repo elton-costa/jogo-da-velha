5547deaf952a18e1089dec3a530c08dc61dad1d5
erdac <tonrc87@gmail.com>
2020-07-15 16:20:47 -0300

    ProfileUser alterado

--- src/components/ProfileUser/index.js
+++ src/components/ProfileUser/index.js
@@ -10,7 +10,7 @@ const ProfileUser = () => (
     <dl className="profile-user">
         <dd className="avatar"><AvatarProfile src={Profile} alt="Avatar do Elton Costa" /></dd>
         <dt className="title">Dev Front-End Jr</dt>
-        <dd className="description">Meu objetivo é encontrar uma oportunidade de emprego, e assim aprofundar e melhorar minhas habilidades em programação.</dd>
+        <dd className="description">Estudando para aprofundar e melhorar as minhas habilidades de programação. No momento, trabalhando para deixar meu código semanticamente correto e de facil manutenção.</dd>
         <dd className="socials" >
             <IconSocial action="https://github.com/elton87" src={GitHub} alt="Logo do GitHub"/>
             <IconSocial action="https://www.linkedin.com/in/elton-costa-a886b51b2" src={Linkedin} alt="Logo do Linkedin"/>
