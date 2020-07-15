264d748e06081c5b5af5f27043349952097e2182
erdac <tonrc87@gmail.com>
2020-07-15 16:29:03 -0300

    ProfileUser alteracao final

--- src/components/ProfileUser/index.js
+++ src/components/ProfileUser/index.js
@@ -9,8 +9,8 @@ import IconSocial from '../../objects/IconSocial';
 const ProfileUser = () => (
     <dl className="profile-user">
         <dd className="avatar"><AvatarProfile src={Profile} alt="Avatar do Elton Costa" /></dd>
-        <dt className="title">Dev Front-End Jr</dt>
-        <dd className="description">Estudando para aprofundar e melhorar as minhas habilidades de programação. No momento, trabalhando para deixar meu código semanticamente correto e de facil manutenção.</dd>
+        <dt className="title">Front-End Júnior</dt>
+        <dd className="description">Estudando para aprofundar e melhorar as minhas habilidades de programação. E assim, deixar cada vez mais, meu código semanticamente correto e de fácil manutenção.</dd>
         <dd className="socials" >
             <IconSocial action="https://github.com/elton87" src={GitHub} alt="Logo do GitHub"/>
             <IconSocial action="https://www.linkedin.com/in/elton-costa-a886b51b2" src={Linkedin} alt="Logo do Linkedin"/>
