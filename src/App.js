91eb49282090ffd974853efd03e7107e1778a855
erdac <tonrc87@gmail.com>
2020-07-16 16:59:07 -0300

    054-18 Show e Hide HistoryGame

--- src/App.js
+++ src/App.js
@@ -11,17 +11,22 @@ import HistoryGame from './components/HistoryGame';
 
 const App = () => {
   const [activeAbout, setActiveAbout] = useState("")
+  const [activeHistoryGame, setHistoryGame] = useState("")
 
   const handleClickAdd = () => setActiveAbout("-active");
   const handleClickRemove = () => setActiveAbout("");
+
+  const handleClick = () => {
+  setHistoryGame("-active")
+}
 
   return (
     <main id="main" className="app">
       <HeaderGame onClick={handleClickAdd}/>
       <HashtagGame />
-      <InputCheckbox id="show" value="show" type="checkbox" content="Mostrar eventos" />
+      <InputCheckbox onClick={handleClick} id="show" value="show" type="checkbox" content="Mostrar eventos" />
 
-      <HistoryGame />
+      <HistoryGame className={activeHistoryGame} />
 
       <LayerDark className={activeAbout}>
         <HeaderInternal onClick={handleClickRemove}/>
